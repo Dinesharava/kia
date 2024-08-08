56a2af54f340a1017ecf3182b1a3bb4dee547411
Dinesharava <dinesharavaa.09@gmail.com>
2024-08-08 10:07:12 +0000

    updated

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@ this is deploy file, where data is stored
 
 this is the djave file to deploy the another branch
 
-this is updated in the branch 
+this is updated in the branch
+
+
+this is for the development check
